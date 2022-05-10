8039c59b2d990dfab23264a44a390dd7386dc47a
Ashton <38200084+Alphatism@users.noreply.github.com>
2022-05-10 01:16:41 -0400

    Fixed TTS

--- loquibot/src/main/java/com/alphalaneous/TTS.java
+++ loquibot/src/main/java/com/alphalaneous/TTS.java
@@ -1,45 +1,11 @@
 package com.alphalaneous;
 
-import javax.sound.sampled.*;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class TTS {
 
     public static void test(String text) {
-
-        AudioInputStream din;
-        try {
-            AudioInputStream in = AudioSystem.getAudioInputStream(new URL("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + URLEncoder.encode(text, StandardCharsets.UTF_8)));
-            AudioFormat baseFormat = in.getFormat();
-            AudioFormat decodedFormat = new AudioFormat(
-                    AudioFormat.Encoding.PCM_SIGNED,
-                    baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
-                    baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
-                    false);
-            din = AudioSystem.getAudioInputStream(decodedFormat, in);
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, decodedFormat);
-            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
-            if(line != null) {
-                line.open(decodedFormat);
-                byte[] data = new byte[4096];
-                // Start
-                line.start();
-
-                int nBytesRead;
-                while ((nBytesRead = din.read(data, 0, data.length)) != -1) {
-                    line.write(data, 0, nBytesRead);
-                }
-                // Stop
-                line.drain();
-                line.stop();
-                line.close();
-                din.close();
-            }
-        }
-        catch(Exception e) {
-                e.printStackTrace();
-            }
+        Sounds.playSound("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + URLEncoder.encode(text, StandardCharsets.UTF_8), false, true, false, true);
     }
 }
